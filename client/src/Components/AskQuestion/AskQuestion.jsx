9fb93f1758f60af684f503f7145235d1b6d7a965
fitse01 <135918799+fitse01@users.noreply.github.com>
2024-03-05 21:38:25 +0300

    Ask Question UI

--- client/src/Components/AskQuestion/AskQuestion.jsx
+++ client/src/Components/AskQuestion/AskQuestion.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import "../../index.css";
+import Header from "../Header/Header";
 
 const AskQuestion = () => {
   return (
-    <div>AskQuestion</div>
-  )
-}
+    <section>
+      <Header />
+      <div className="container d-flex flex-column align-items-center mt-4 ">
+        {/* steps and how to write Q  */}
+        <div className="justify-content-around ">
+          {/*  how to write Q  Title*/}
+          <h2>Steps to Write a good questions </h2>
+        </div>
 
-export default AskQuestion
+        <div>
+          {/*step to write Q  */}
+          <ul>
+            <li> Summarise you problem in one-line title</li>
+            <li> Describe your problem in more detail</li>
+            <li>Describe what you tried and What you expected to happen </li>
+            <li>Review your question and post it to the site </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="d-flex flex-column align-items-center container  shadow-sm p-3 mb-5 bg-body rounded">
+        <div className="mt-5 pt-4">
+          {/* Ask Q Part */}
+          <div>
+            <h3>Ask a Public question </h3>
+          </div>
+          <div className="align-items-center">
+            <p>Go to Question page</p>
+          </div>
+        </div>
+
+        <div className="container">
+          {/* form part */}
+          <form action="" className="">
+            <div className="mb-2">
+              <input
+                type="text"
+                placeholder="Title"
+                className="form-control "
+              />
+            </div>
+            <div cla>
+              {/* <input
+              type="text"
+              placeholder="Question Description "
+              className="form-control py-5  "
+            /> */}
+              <textarea
+                class="form-control p-4"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                placeholder="Question Description "
+              ></textarea>
+            </div>
+            <div className=" mt-2">
+              <button
+                className="btn btn-primary fw-bold px-5 action_btn"
+                type="Submit"
+              >
+                Post your Question
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AskQuestion;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,14 +3,20 @@ import Header from "../../Components/Header/Header";
 import "../../index.css";
 import Question from "../../Components/Question/Question";
 import AskQuestion from "../../Components/AskQuestion/AskQuestion";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate =useNavigate();
+
+  const handleAskQuestionClick = () => {
+    navigate('/questions'); // Navigates to the "/questions" route
+  };
   return (
     <>
       <Header />
       <section className=" bg-body-tertiary">
         <div className="d-flex justify-content-around pt-5  ">
-          <a href="#" onClick={AskQuestion} className="btn btn-primary action_btn px-5">
+          <a href="#"  onClick={handleAskQuestionClick} className="btn btn-primary action_btn px-5">
             {" "}
             Ask Question{" "}
           </a>
